3fa851f0fe47325a18cd7031c99b64aebd21a446
yeonsookang <yeonsoo.kang@lapiscine.pro>
2023-11-21 14:24:52 +0100

    correction of page starsDetailPage

--- src/pages/StarsDetailPage.jsx
+++ src/pages/StarsDetailPage.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { useParams } from 'react-router-dom'
 import { stars } from '../utils/utils'
-import CelebrityCard from '../components/CelebrityCard'
+// import CelebrityCard from '../components/CelebrityCard'
 
 function StarsDetailPage (){
 
@@ -16,7 +16,16 @@ function StarsDetailPage (){
         <>
             <Header/>
             <main>
-                {<CelebrityCard star={stardetail}/>}
+                {/* {<CelebrityCard star={stardetail}/>} */}
+                {/* dans cette page, on a pas besoin de re-utiliser notre composant Celebrtycard, parce que deja, on a plus besoin de Link,
+                et cette page est cence voir tout les infos de stars. donc ici, on cree normalement */}
+
+                <article className='celebritycard--article'>
+                    <h3>{stardetail.name}</h3>
+                    <img src={stardetail.img} alt={stardetail.name} />
+                    <p>{stardetail.bio}</p>
+                    <p>{stardetail.publishedAt}</p>
+                </article>
             </main>
             <Footer/>
         </>
